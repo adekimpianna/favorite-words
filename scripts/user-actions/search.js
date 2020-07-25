ca71ce50ee9a9169d21e7ced139bd1654c2c8d27
Yana <adek.impianna@gmail.com>
2020-07-25 21:18:48 +0200

    Completed Function: Search Words

--- scripts/user-actions/search.js
+++ scripts/user-actions/search.js
@@ -3,15 +3,22 @@ console.log('--- loading: search.js');
 
 const searchHandler = () => {
   debugger;
-  const userInput = prompt('');
-  if (_) {
+  const userInput = prompt('enter a search query:');
+  if ( userInput == null) {
     return;
   }
 
   const includesSubstring = (entry) => {
-    return _;
+    let result = ''
+    if (entry.includes(userInput)) {
+      return entry;
+    }
+    else {
+      return result;
+    }
   };
-  const foundWords = words._(_);
 
-  displayHandler(``, _);
+  const foundWords = words.map(includesSubstring);
+
+  displayHandler(`search result for ${userInput}`, foundWords);
 };
